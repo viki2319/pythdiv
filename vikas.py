aef85a4bbfe91f4df60cbaeb943d03eb05393c9a
Vikas V <vikas.v@upswing.one>
2025-05-24 12:28:53 +0530

    Update line 6 in vikas.py via script

--- vikas.py
+++ vikas.py
@@ -3,7 +3,10 @@ from datetime import datetime
 def make_code_changes(repo_path):
     filepath = os.path.join(repo_path, "solution.txt")
     code = f"Auto-generated code updated at {datetime.now().isoformat()}\n"
-    with open(filepath, "w") as f:
+suspend fun getRewards(accountId: AccountId)
+{
+    return RewardDao.getRewardForAccount(accountId)
+}
         f.write(code)
 byy
 vikas
